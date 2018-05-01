97e21ff37a5935a57be108ecf680ddaf78660ea2
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-05-01 19:51:11 +0800

    完成Library功能

--- src/main/java/com/tw/Library.java
+++ src/main/java/com/tw/Library.java
@@ -1,7 +1,114 @@
 package com.tw;
 
+import com.google.common.collect.Sets;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Library {
+
+    private String inputStudentFormate = "（格式：姓名, 学号, 学科: 成绩, ...）";
+    private String generateFormate = "（格式： 学号, 学号,...）";
+
+    private Set<Student> students = new LinkedHashSet<>();
+
+    private Asker asker;
+
+    public void serve() {
+        int option = asker.askForInteger(Constants.MAIN_MENU);
+        switch (option) {
+            case 1:
+                inputStudent();
+                break;
+            case 2:
+                queryGradeList();
+                break;
+            case 3:
+                return ;
+            default:
+                break;
+        }
+        serve();
+    }
+
+    private void queryGradeList() {
+        String ids = asker.askForLine(Constants.QUERY_GRADE_LIST_MESSAGE);
+        HashSet<String> idSet = Sets.newHashSet(ids.split(", "));
+        String table = Constants.GRADE_LIST_HEADER;
+        double sum = 0;
+        Set<Student> studentSet = new HashSet<>();
+        for (Student stu : students) {
+            if (!idSet.contains(stu.getId())) {
+                continue;
+            }
+            table += stu.toString() + "\n";
+            sum += stu.getTotal();
+            studentSet.add(stu);
+        }
+        //求中位数
+        Double[] gradeList = new Double[studentSet.size()];
+        int i = 0;
+        for (Student stu : studentSet) {
+            gradeList[i++] = stu.getTotal();
+        }
+        Arrays.sort(gradeList);
+        double mid = gradeList.length % 2 == 1 ? gradeList[gradeList.length/2] : (gradeList[gradeList.length/2 - 1] + gradeList[gradeList.length/2]) / 2;
+        table += String.format("========================\n" +
+                               "全班总分平均数：%s\n" +
+                               "全班总分中位数：%s\n",
+                               StringUtils.doubleToString(sum/idSet.size()),
+                               StringUtils.doubleToString(mid));
+        System.out.print(table);
+    }
+
+    private void inputStudent() {
+        String line = asker.askForLine(Constants.STDDENT_INPUT_MESSAGE);
+        while (!addStudent(line)) {
+            line = asker.askForLine(Constants.STUDENT_INPUT_FORMAT_ERROR);
+        }
+    }
+
+    public boolean addStudent(String line){
+        Student student = Parser.parseLineToStudent(line);
+        if (student == null) {
+            return false;
+        }
+        students.add(student);
+        return true;
+    }
+
+    public Library(Asker asker) {
+        this.asker = asker;
+    }
+
+    public Library() {
+        this(new Asker(System.in, System.out));
+    }
+
     public boolean someLibraryMethod() {
         return true;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * 处理double类型末尾的0
+     *
+     * @param s
+     * @return
+     */
+    private static String subZeroAndDot(String s) {
+        if (null == s) {
+            return "";
+        }
+        if (s.indexOf(".") > 0) {
+            s = s.replaceAll("0+?$", "");//去掉多余的0
+            s = s.replaceAll("[.]$", "");//如最后一位是.则去掉
+        }
+        return s;
     }
 }
--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,15 +1,33 @@
 package com.tw;
 
+import com.google.common.collect.Sets;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.LinkedList;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LibraryTest {
+
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream(1);
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
     @Test
     public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
@@ -28,5 +46,85 @@ public class LibraryTest {
         assertEquals(mockedList.get(0), value);
 
     }
+
+    @Test
+    public void test_input_student_success() {
+        Student testStudent1 = new Student("001", "白云飞", 100.0,100.0,100.0,100.0);
+
+        Asker asker = mock(Asker.class);
+        when(asker.askForInteger(Constants.MAIN_MENU)).thenReturn(1,3);
+        when(asker.askForLine(Constants.STDDENT_INPUT_MESSAGE)).thenReturn(praseStudentToInputString(testStudent1));
+
+        Library library = new Library(asker);
+        library.serve();
+        assertThat(library.getStudents()).contains(testStudent1);
+    }
+
+    @Test
+    public void test_calculate_average_and_total_grade() {
+        Student testStudent1 = new Student("001", "白云飞", 100.0,100.0,100.0,100.0);
+
+        Asker asker = mock(Asker.class);
+        when(asker.askForInteger(Constants.MAIN_MENU)).thenReturn(1,3);
+        when(asker.askForLine(Constants.STDDENT_INPUT_MESSAGE)).thenReturn(praseStudentToInputString(testStudent1));
+
+        Library library = new Library(asker);
+        library.serve();
+
+        for (Student stu : library.getStudents()) {
+            double total = stu.getChinese() + stu.getMath() + stu.getEnglish() + stu.getProgramming();
+            double avg = (total) / 4;
+            assertThat(stu.getTotal()).isEqualTo(total);
+            assertThat(stu.getAverage()).isEqualTo(avg);
+        }
+    }
+
+    @Test
+    public void test_generate_grade_list(){
+        Student testStudent1 = new Student("001", "张三", 75.0,95.0,80.0,80.0);
+        Student testStudent2 = new Student("002", "李四", 85.0, 80.0, 70.0, 90.0);
+
+        Asker asker = mock(Asker.class);
+        when(asker.askForInteger(Constants.MAIN_MENU)).thenReturn(1,1,2,2,3);
+        when(asker.askForLine(Constants.STDDENT_INPUT_MESSAGE))
+                .thenReturn(praseStudentToInputString(testStudent1),
+                            praseStudentToInputString(testStudent2));
+        when(asker.askForLine(Constants.QUERY_GRADE_LIST_MESSAGE)).thenReturn("001, 002","001");
+
+        Library library = new Library(asker);
+        library.serve();
+
+        assertThat(systemOut()).isEqualTo("成绩单\n" +
+                "姓名|数学|语文|英语|编程|平均分|总分\n" +
+                "========================\n" +
+                "张三|75|95|80|80|82.5|330\n" +
+                "李四|85|80|70|90|81.25|325\n" +
+                "========================\n" +
+                "全班总分平均数：327.5\n" +
+                "全班总分中位数：327.5\n"+
+                "成绩单\n" +
+                "姓名|数学|语文|英语|编程|平均分|总分\n" +
+                "========================\n" +
+                "张三|75|95|80|80|82.5|330\n" +
+                "========================\n" +
+                "全班总分平均数：330\n" +
+                "全班总分中位数：330\n");
+    }
+
+    /**
+     * 将Student实例转换成输入格式的字符串，方便测试用
+     * @param student
+     * @return
+     */
+    private String praseStudentToInputString(Student student){
+        return String.format("%s, %s, 数学: %f, 英语: %f, 语文: %f, 编程: %f",
+                student.getName(),
+                student.getId(),
+                student.getMath(),
+                student.getEnglish(),
+                student.getChinese(),
+                student.getProgramming());
+    }
+
 
 }
